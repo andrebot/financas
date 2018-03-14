db0e72620a2240e463595a859305768b05c71e34
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2018-03-14 18:33:04 -0300

    Adding the public folder. Adding Material Design package

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,8 @@ const database = require('./database');
 
 const app = express();
 
+app.use(express.static('public'));
+
 database.connectToDb(function () {
   app.listen(serverConfig.PORT, function () {
     console.log(`Listening on ${serverConfig.PORT}...`);
