acd6a048f032f9eac97bcf9b7b66e0a178fa7ae5
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2018-03-20 17:30:08 -0300

    Added first list all test

--- src/server/controllers/controller.spec.js
+++ src/server/controllers/controller.spec.js
@@ -11,7 +11,8 @@ describe('Controller', function () {
     };
 
     const fakeCalls = {
-      save: sinon.stub().returns(fakePromise)
+      save: sinon.stub().returns(fakePromise),
+      find: sinon.stub().returns(fakePromise)
     };
 
     this.fakeCalls = fakeCalls;
@@ -25,6 +26,7 @@ describe('Controller', function () {
       }
     };
     this.fakeModel.collection = { name: 'TestFake' };
+    this.fakeModel.find = fakeCalls.find;
 
     this.expressMocks = {
       request: {},
@@ -37,64 +39,88 @@ describe('Controller', function () {
     throwNewError = false;
   });
 
-  it('should save successfully a generic model', function (done) {
-    const { request, response } = this.expressMocks;
-    const dummyData = 'hey';
+  describe('create', function () {
+    it('should save successfully a generic model', function (done) {
+      const { request, response } = this.expressMocks;
+      const dummyData = 'hey';
 
-    this.fakePromise.then.callsArgWith(0, { dummyData, _id: 1 });
+      this.fakePromise.then.callsArgWith(0, { dummyData, _id: 1 });
 
-    request.body = { dummyData };
-    response.json = result => {
-      this.fakeCalls.save.should.have.been.calledOnce;
-      this.fakePromise.then.should.have.been.calledOnce;
-      this.fakePromise.catch.should.have.not.been.called;
+      request.body = { dummyData };
+      response.json = result => {
+        this.fakeCalls.save.should.have.been.calledOnce;
+        this.fakePromise.then.should.have.been.calledOnce;
+        this.fakePromise.catch.should.have.not.been.called;
 
-      result.should.be.an('Object');
-      result.should.have.property('data');
-      result.data.should.exist;
+        result.should.be.an('Object');
+        result.should.have.property('data');
+        result.data.should.exist;
 
-      done();
-    };
+        done();
+      };
 
-    this.controller.create(request, response);
+      this.controller.create(request, response);
+    });
+
+    it('should send the client an error if anything goes wrong in the database while saving', function(done) {
+      const { request, response } = this.expressMocks;
+      const dummyData = 'hey';
+
+      this.fakePromise.catch.callsArgWith(0, new Error('Dumb error'));
+      request.body = { dummyData };
+      response.send = message => {
+        this.fakeCalls.save.should.have.been.calledOnce;
+        this.fakePromise.catch.should.have.been.calledOnce;
+
+        message.should.be.a('String');
+        message.should.not.be.empty;
+
+        done();
+      };
+
+      this.controller.create(request, response);
+    });
+
+    it('should send the client an error if the info provided does not match', function(done) {
+      const { request, response } = this.expressMocks;
+
+      throwNewError = true;
+
+      response.send = message => {
+        this.fakeCalls.save.should.have.not.been.called;
+        this.fakePromise.catch.should.have.not.been.called;
+  
+        response.status.should.have.been.calledWith(500);
+        message.should.exist;
+        message.should.be.an('string');
+        message.should.not.be.empty;
+
+        done();
+      }
+
+      this.controller.create(request, response);
+    });
   });
 
-  it('should send the client an error if anything goes wrong in the database while saving', function(done) {
-    const { request, response } = this.expressMocks;
-    const dummyData = 'hey';
+  describe('list all', function () {
+    it('should list all documents of a model', function (done) {
+      const { request, response } = this.expressMocks;
+      const dummyResult = [{dummyData: 'hey'}];
 
-    this.fakePromise.catch.callsArgWith(0, new Error('Dumb error'));
-    request.body = { dummyData };
-    response.send = message => {
-      this.fakeCalls.save.should.have.been.calledOnce;
-      this.fakePromise.catch.should.have.been.calledOnce;
+      this.fakePromise.then.callsArgWith(0, dummyResult);
 
-      message.should.be.a('String');
-      message.should.not.be.empty;
+      response.json = result => {
+        result.should.exist;
+        result.should.be.an('object');
+        result.should.own.property('data');
+        result.data.should.be.an('array');
+        result.data.should.not.be.empty;
+        result.data.length.should.be.eq(dummyResult.length);
 
-      done();
-    };
+        done();
+      };
 
-    this.controller.create(request, response);
-  });
-
-  it('should send the client an error if the info provided does not match', function(done) {
-    const { request, response } = this.expressMocks;
-
-    throwNewError = true;
-
-    response.send = message => {
-      this.fakeCalls.save.should.have.not.been.called;
-      this.fakePromise.catch.should.have.not.been.called;
-
-      response.status.should.have.been.calledWith(500);
-      message.should.exist;
-      message.should.be.an('string');
-      message.should.not.be.empty;
-
-      done();
-    }
-
-    this.controller.create(request, response);
+      this.controller.listAll(request, response);
+    });
   });
 });
