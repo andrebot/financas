6c6abb3f96afa75a6acab28f67a9b09b2b2fa96d
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2018-04-07 13:56:10 -0300

    Added jsdom to solve mount issue iwth enzyme. React tests fully working

--- src/client/components/table/table.spec.js
+++ src/client/components/table/table.spec.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import sinon from 'sinon';
 import Table from './table.jsx';
 
 describe('<Table />', function () {
   it('renders', function () {
+    const headers = [{title: 'dummy', mapTo: 'a'}];
+    const data = [{a: 'dummy'}];
+    const table = mount(<Table headers={headers} data={data}/>);
+    table.find('tr').length.should.be.eq(2);
     
   });
 });
--- tests/client/imports.js
+++ tests/client/imports.js
@@ -2,6 +2,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Chai from 'chai';
 import Sinon from 'sinon-chai';
+import 'jsdom-global/register';
 
 Enzyme.configure({ adapter: new Adapter() });
 Chai.should();
