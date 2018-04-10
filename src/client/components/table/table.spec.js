944bd2c9365cca36328c060b906d83a928f5075c
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2018-04-10 19:09:57 -0300

    Added hapy path test for table.jsx

--- src/client/components/table/table.spec.js
+++ src/client/components/table/table.spec.js
@@ -4,11 +4,15 @@ import sinon from 'sinon';
 import Table from './table.jsx';
 
 describe('<Table />', function () {
-  it('renders', function () {
+  it('should render two rolls, one for header and one for data', function () {
     const headers = [{title: 'dummy', mapTo: 'a'}];
-    const data = [{a: 'dummy'}];
+    const data = [{ a: { value: 'dummy data', type: 'String' } }];
     const table = mount(<Table headers={headers} data={data}/>);
+
     table.find('tr').length.should.be.eq(2);
-    
+    table.find('th').length.should.be.eq(1);
+    table.find('td').length.should.be.eq(1);
+    table.find('th').text().should.be.eq(headers[0].title);
+    table.find('td').text().should.be.eq(data[0].a.value);
   });
 });
