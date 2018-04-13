b17f8c767bd70a84215ebe7721982eb0598b60d1
Andre Botelho Rodrigues Almeida <andrebot_almeida@hotmail.com>
2018-04-13 18:52:29 -0300

    Added currency parsing test

--- src/client/components/table/formatHelpers.spec.js
+++ src/client/components/table/formatHelpers.spec.js
@@ -8,7 +8,7 @@ describe('FormatHelpers', function () {
       this.dateRegExp = /[0-2]\d\/[0-2]\d\/\d\d\d\d/;
     });
 
-    it('should parse data correctly to dd/mm/YYYY', function () {
+    it('should parse data correctly to `dd/mm/YYYY`', function () {
       const formattedDate = formatDate(new Date());
 
       formattedDate.should.not.be.empty;
@@ -17,6 +17,24 @@ describe('FormatHelpers', function () {
 
     it('should throw error if trying to parse something that is not a date', function () {
       expect(() => formatDate()).to.throw(Error);
+    });
+  });
+
+  describe('formatCurrency', function () {
+    before(function () {
+      this.currencyRegExp = /R\$ [0-9]+.\d\d/;
+    });
+
+    it('should parse data correctly to `R$ [0-9]+.DD`', function () {
+      const formattedCurrency = formatCurrency(1);
+
+      formattedCurrency.should.not.be.empty;
+      this.currencyRegExp.test(formattedCurrency).should.be.true;
+    });
+
+    it('should throw error if trying to parse something that is not a number', function () {
+      expect(() => formatCurrency()).to.throw(Error);
+      expect(() => formatCurrency('123123')).to.throw(Error);
     });
   });
 });
