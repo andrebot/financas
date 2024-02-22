898977d4c04551f2c8311ac4e5b7664406113b40
Andre Almeida <andrebot_almeida@hotmail.com>
2024-02-21 23:30:10 -0500

    Adding JSDocs

--- src/server/managers/authenticationManager.ts
+++ src/server/managers/authenticationManager.ts
@@ -15,13 +15,30 @@ import {
   REFRESH_TOKEN_EXPIRATION,
 } from '../config/auth';
 
+/**
+ * Function to create a token
+ *
+ * @param payload - Object to be added to the token
+ * @param expiresIn - Time for the token to expire
+ * @param secret - Secret to be used to create the token
+ * @returns - the Token as a string
+ */
 export function createToken(payload: UserPayload, expiresIn: string, secret: string): string {
   return jwt.sign(payload, secret, { issuer: ISSUER, expiresIn });
 }
 
+/**
+ * Function to create an access token
+ *
+ * @param email - Email of the user to be added to the token
+ * @param role - Role of the user to be added to the token
+ * @param firstName - First name of the user to be added to the token
+ * @param lastName - Last name of the user to be added to the token
+ * @returns - the Access Token as a string
+ */
 export function createAccessToken(
   email: string,
-  role: string,
+  role: 'admin' | 'user',
   firstName: string,
   lastName: string,
 ): string {
@@ -33,10 +50,28 @@ export function createAccessToken(
   }, ACCESS_TOKEN_EXPIRATION, ACCESS_TOKEN_SECRET);
 }
 
+/**
+ * Function to create a refresh token
+ *
+ * @param email - Email of the user to be added to the token
+ * @returns - the Refresh Token as a string
+ */
 export function createRefreshToken(email: string): string {
   return createToken({ email }, REFRESH_TOKEN_EXPIRATION, REFRESH_TOKEN_SECRET);
 }
 
+/**
+ * Function to create a user. It will hash the password and add the user to the database
+ *
+ * @throws - Error if the password does not follow the rules
+ *
+ * @param email - Email of the user to be added to the database
+ * @param password - Password of the user to be added to the database
+ * @param firstName - First name of the user to be added to the database
+ * @param lastName - Last name of the user to be added to the database
+ * @param role - Role of the user to be added to the database
+ * @returns - the User as an object
+ */
 export async function createUser(email: string, password: string, firstName: string, lastName: string, role = 'user'): Promise<IUser> {
   if (regExpPassword.test(password)) {
     const salt = bcrypt.genSaltSync(WORK_FACTOR);
@@ -55,6 +90,16 @@ export async function createUser(email: string, password: string, firstName: str
   throw new Error('Password does not follow the rules');
 }
 
+/**
+ * Function to update a user. It will find the user by id and update the information provided
+ *
+ * @throws - Error if no information is provided to be updated
+ * @throws - Error if the user is not found
+ *
+ * @param id - Id of the user to be updated
+ * @param payload - Information to be updated
+ * @returns - the User as an object
+ */
 export async function updateUser(id: string, payload: UserPayload): Promise<IUser> {
   const user = await UserModel.findById(id);
 
@@ -88,10 +133,24 @@ export async function updateUser(id: string, payload: UserPayload): Promise<IUse
   throw new Error('User not found');
 }
 
+/**
+ * Function to list users. It will find the users based on the query provided
+ *
+ * @param query - Query to be used to find the users
+ * @returns - the Users as an array
+ */
 export function listUsers(query: FilterQuery<IUser> = {}): Promise<IUser[]> {
   return UserModel.find(query);
 }
 
+/**
+ * Function to delete a user. It will find the user by id and delete it
+ *
+ * @throws - Error if the user is not found
+ *
+ * @param id - Id of the user to be deleted
+ * @returns - the User as an object
+ */
 export async function deleteUser(id: string): Promise<IUser> {
   return UserModel.findByIdAndDelete(id).then((deletedUser) => {
     if (!deletedUser) {
@@ -102,6 +161,16 @@ export async function deleteUser(id: string): Promise<IUser> {
   });
 }
 
+/**
+ * Function to login. It will find the user by email and compare the password provided
+ *
+ * @throws - Error if the password does not match
+ * @throws - Error if the user is not found
+ *
+ * @param searchEmail - Email of the user to be found
+ * @param password - Password of the user to be compared
+ * @returns - the Tokens as an object
+ */
 export async function login(searchEmail: string, password: string): Promise<Tokens> {
   const user = await UserModel.findOne({ searchEmail });
 
@@ -131,21 +200,39 @@ export async function login(searchEmail: string, password: string): Promise<Toke
   throw new Error('User not found');
 }
 
+/**
+ * Function to logout. It will delete the token from the database.
+ *
+ * @param refreshToken - Refresh Token to be deleted
+ * @returns - a boolean
+ */
 export async function logout(refreshToken: string): Promise<boolean> {
+  let verification = false;
+
   try {
     jwt.verify(refreshToken, REFRESH_TOKEN_SECRET, { complete: true }) as Token;
 
-    deleteToken(refreshToken);
-
-    return true;
+    verification = true;
   } catch (error) {
-    Logger.error('Failed to verify token');
+    Logger.error('Failed to verify token. Invalidating it anyway.');
     Logger.error(error);
-
-    return false;
+  } finally {
+    deleteToken(refreshToken);
   }
+
+  return verification;
 }
 
+/**
+ * Function to refresh tokens. It will verify the token and create new
+ * access and refresh tokens
+ *
+ * @throws - Error if the token is not valid
+ * @throws - Error if the user is not found
+ *
+ * @param refreshToken - Refresh Token to be verified
+ * @returns - the Tokens as an object
+ */
 export async function refreshTokens(refreshToken: string): Promise<Tokens> {
   if (isValidToken(refreshToken)) {
     const tokenDecrypted = jwt.verify(
--- src/server/resources/userModel.ts
+++ src/server/resources/userModel.ts
@@ -7,18 +7,47 @@ import {
 } from 'mongoose';
 import { regExpEmail } from '../utils/validators';
 
+/**
+ * Interface for the User
+ */
 export interface IUser extends Document{
+  /**
+   * Email of the user
+   */
   email: string;
+  /**
+   * First name of the user
+   */
   firstName: string;
+  /**
+   * Last name of the user
+   */
   lastName: string;
+  /**
+   * Role of the user. Can be either 'admin' or 'user'
+   */
   role: 'admin' | 'user';
+  /**
+   * Password of the user
+   */
   password: string;
 }
 
+/**
+ * Type for the User. It is a Document and a FlatRecord of the IUser to follow
+ * the schema and a _id of type ObjectId on Mongoose
+ */
 type DocType = Document<unknown, unknown, FlatRecord<IUser>> & FlatRecord<IUser> & {
   _id: Types.ObjectId;
 };
 
+/**
+ * Function to transform the user object by removing the password, since it should not be returned
+ *
+ * @param doc - Document of the user
+ * @param ret - Record of the user
+ * @returns - Record of the user
+ */
 function transformUserObject(doc: DocType, ret: Record<string, unknown>) {
   const { ...newObject } = ret;
 
@@ -27,6 +56,9 @@ function transformUserObject(doc: DocType, ret: Record<string, unknown>) {
   return newObject;
 }
 
+/**
+ * Schema for the User
+ */
 const UserModel = new Schema<IUser>({
   email: {
     type: String, match: regExpEmail, unique: true, required: true,
