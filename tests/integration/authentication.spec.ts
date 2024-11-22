1c69bd59629ab9bfe709e78f4e4434eb284a985b
Andre Almeida <andrebot_almeida@hotmail.com>
2024-11-21 23:26:14 -0500

    Fixing authentication integration tests

--- tests/integration/authentication.spec.ts
+++ tests/integration/authentication.spec.ts
@@ -1,16 +1,16 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import jwt from 'jsonwebtoken';
+import { Types } from 'mongoose';
 import server from '../../src/server/server';
 import { adminUser } from './connectDB';
 import { createAccessToken, createRefreshToken } from '../../src/server/managers/authenticationManager';
 import UserModel from '../../src/server/resources/models/userModel';
 import { addToken, deleteToken } from '../../src/server/resources/repositories/tokenRepo';
-import { Types } from 'mongoose';
 
 describe('Authentication', () => {
   let newUser = {
-    _id: '',
+    id: '',
     email: 'test1@gmail.com',
     firstName: 'Test1',
     lastName: 'User1',
@@ -34,7 +34,7 @@ describe('Authentication', () => {
         'admin',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
 
       chai.request(server)
@@ -55,7 +55,7 @@ describe('Authentication', () => {
         'user',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
 
       chai.request(server)
@@ -73,7 +73,7 @@ describe('Authentication', () => {
         'admin',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
       const stub = sinon.stub(UserModel, 'find').throws();
 
@@ -99,7 +99,7 @@ describe('Authentication', () => {
         'admin',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
     });
 
@@ -123,10 +123,10 @@ describe('Authentication', () => {
           res.body.should.have.property('firstName').eql(newUser.firstName);
           res.body.should.have.property('lastName').eql(newUser.lastName);
           res.body.should.have.property('role').eql('user');
-          res.body.should.have.property('_id');
+          res.body.should.have.property('id');
           res.body.should.not.have.property('password');
 
-          newUser._id = res.body._id;
+          newUser.id = res.body.id;
           done();
         });
     });
@@ -137,7 +137,7 @@ describe('Authentication', () => {
         'user',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
 
       chai.request(server)
@@ -230,7 +230,7 @@ describe('Authentication', () => {
         'user',
         newUser.firstName,
         newUser.lastName,
-        newUser._id,
+        newUser.id!,
       );
     });
 
@@ -240,18 +240,17 @@ describe('Authentication', () => {
         'admin',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
       adminUser.firstName.should.be.eql('Admin');
 
       chai.request(server)
-        .put(`/api/v1/user/${adminUser._id}`)
+        .put(`/api/v1/user/${adminUser.id!}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ firstName: 'Admin1' })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property('firstName').eql('Admin1');
-          res.body.should.not.have.property('password');
           done();
         });
     });
@@ -266,7 +265,7 @@ describe('Authentication', () => {
       );
 
       chai.request(server)
-        .put(`/api/v1/user/${newUser._id}`)
+        .put(`/api/v1/user/${newUser.id!}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ firstName: 'Test2' })
         .end((err, res) => {
@@ -278,7 +277,7 @@ describe('Authentication', () => {
 
     it('should not be able to update password of an user if it is the same user', (done) => {
       chai.request(server)
-        .put(`/api/v1/user/${newUser._id}`)
+        .put(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ password: 'Gero-jun23' })
         .end((err, res) => {
@@ -290,7 +289,7 @@ describe('Authentication', () => {
 
     it('should not be able to update role of an user if it is the same user', (done) => {
       chai.request(server)
-        .put(`/api/v1/user/${newUser._id}`)
+        .put(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ role: 'admin' })
         .end((err, res) => {
@@ -302,13 +301,12 @@ describe('Authentication', () => {
 
     it('should be able to update firstName of an user if it is the same user', (done) => {
       chai.request(server)
-        .put(`/api/v1/user/${newUser._id}`)
+        .put(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ firstName: 'Test2' })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property('firstName').eql('Test2');
-          res.body.should.not.have.property('password');
 
           done();
         });
@@ -316,13 +314,12 @@ describe('Authentication', () => {
 
     it('should be able to update lastName of an user if it is the same user', (done) => {
       chai.request(server)
-        .put(`/api/v1/user/${newUser._id}`)
+        .put(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ lastName: 'bogabofa' })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property('lastName').eql('bogabofa');
-          res.body.should.not.have.property('password');
 
           done();
         });
@@ -332,13 +329,12 @@ describe('Authentication', () => {
       const newEmail = 'new@email.com';
 
       chai.request(server)
-        .put(`/api/v1/user/${newUser._id}`)
+        .put(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ email: newEmail })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.have.property('email').eql(newEmail);
-          res.body.should.not.have.property('password');
 
           newUser.email = newEmail;
           done();
@@ -349,7 +345,7 @@ describe('Authentication', () => {
       const stub = sinon.stub(UserModel, 'findById').throws();
 
       chai.request(server)
-        .put(`/api/v1/user/${newUser._id}`)
+        .put(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .send({ email: 'nada@gmail.com' })
         .end((err, res) => {
@@ -384,7 +380,7 @@ describe('Authentication', () => {
         'admin',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
     });
 
@@ -394,11 +390,11 @@ describe('Authentication', () => {
         'user',
         newUser.firstName,
         newUser.lastName,
-        newUser._id,
+        newUser.id!,
       );
 
       chai.request(server)
-        .delete(`/api/v1/user/${newUser._id}`)
+        .delete(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
           res.should.have.status(403);
@@ -408,7 +404,7 @@ describe('Authentication', () => {
 
     it('should return a 401 error if the user is not authenticated', (done) => {
       chai.request(server)
-        .delete(`/api/v1/user/${newUser._id}`)
+        .delete(`/api/v1/user/${newUser.id}`)
         .end((err, res) => {
           res.should.have.status(401);
           done();
@@ -429,23 +425,23 @@ describe('Authentication', () => {
 
     it('should be able to delete an user if it is an admin', (done) => {
       chai.request(server)
-        .delete(`/api/v1/user/${newUser._id}`)
+        .delete(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.have.property('message').eql(`User deleted: id ${newUser._id}`);
+          res.body.should.have.property('message').eql(`User deleted: id ${newUser.id}`);
 
           done();
         });
     });
 
-    it('should return error if the user is not found', (done) => {
+    it('should return 404 if the user is not found', (done) => {
       chai.request(server)
-        .delete(`/api/v1/user/${newUser._id}`)
+        .delete(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
-          res.should.have.status(500);
-          res.body.should.have.property('error').eql(`User not found ${newUser._id}`);
+          res.should.have.status(404);
+          res.body.should.have.property('error').eql(`User not found ${newUser.id}`);
 
           done();
         });
@@ -455,7 +451,7 @@ describe('Authentication', () => {
       const stub = sinon.stub(UserModel, 'findByIdAndDelete').throws();
 
       chai.request(server)
-        .delete(`/api/v1/user/${newUser._id}`)
+        .delete(`/api/v1/user/${newUser.id}`)
         .set('Authorization', `Bearer ${token}`)
         .end((err, res) => {
           res.should.have.status(500);
@@ -468,12 +464,12 @@ describe('Authentication', () => {
   });
 
   describe('Logging in - POST /api/v1/login', () => {
-    it('should return a 500 error if the user is not found', (done) => {
+    it('should return a 404 error if the user is not found', (done) => {
       chai.request(server)
         .post('/api/v1/user/login')
         .send({ email: 'andre.almeida@gmail.com', password: 'Maka-jan32' })
         .end((err, res) => {
-          res.should.have.status(500);
+          res.should.have.status(404);
           res.body.should.have.property('error').eql('User not found');
           done();
         });
@@ -590,7 +586,7 @@ describe('Authentication', () => {
         'admin',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
 
       addToken(refreshToken);
@@ -674,7 +670,7 @@ describe('Authentication', () => {
     let token: string;
 
     before(async () => {
-      await UserModel.findByIdAndUpdate(adminUser._id, { email: adminUser.email });
+      await UserModel.findByIdAndUpdate(adminUser.id!, { email: adminUser.email });
     });
 
     beforeEach(() => {
@@ -683,7 +679,7 @@ describe('Authentication', () => {
         'admin',
         adminUser.firstName,
         adminUser.lastName,
-        adminUser._id,
+        adminUser.id!,
       );
     });
 
