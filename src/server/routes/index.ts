d65ca08bc707c7727530c4781d8899ec8eb5a221
Andre Almeida <andrebot_almeida@hotmail.com>
2025-04-10 14:37:49 -0400

    Adding some docs

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -15,6 +15,12 @@ import type { IAccount, IBudget, ICategory, IGoal, IContentController } from '..
 
 const logger = createLogger('RoutesInitializer');
 
+/**
+ * Standard routes are the routes that are created by the standardRouteFactory.
+ * For controllers that are just require standard CRUD operations
+ * 
+ * @see {@link standardRouteFactory}
+ */
 const standardRoutes: { prefix: string; controller: IContentController }[] = [
   standardRouteFactory<IAccountDocument, IAccount>(AccountModel, 'account'),
   standardRouteFactory<ICategoryDocument, ICategory>(CategoryModel, 'category'),
@@ -26,6 +32,13 @@ const standardRoutes: { prefix: string; controller: IContentController }[] = [
     contentManager: BudgetManager,
   }),
 ];
+
+/**
+ * Custom routes are the routes that are created by the customRouteFactory.
+ * For controllers that are require custom logic
+ * 
+ * @see {@link customRouteFactory}
+ */
 const customRoutes: { prefix: string; router: Router }[] = [
   {
     prefix: 'user',
@@ -37,11 +50,23 @@ const customRoutes: { prefix: string; router: Router }[] = [
   },
 ];
 
+/**
+ * Adds a route to the express app
+ * 
+ * @param app - The express app
+ * @param router - The router to add
+ * @param prefix - The prefix to add
+ */
 function addRoute(app: Express, router: Router, prefix: string) {
   app.use(`${API_PREFIX}/${prefix}`, router);
   logger.info(`Route added: ${API_PREFIX}/${prefix}`);
 }
 
+/**
+ * Sets the routes for the express app
+ * 
+ * @param app - The express app
+ */
 export default function setRoutes(app: Express) {
   standardRoutes.forEach((route) => {
     addRoute(app, contentRouterFactory(route.controller), route.prefix);
